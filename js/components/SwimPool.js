9556f235312a87eafb31fa8d990b861427dbaffe
junqing <1551486328@qq.com>
2018-01-02 14:04:33 +0800

    game

--- js/components/SwimPool.js
+++ js/components/SwimPool.js
@@ -43,22 +43,22 @@ export default class SwimPool extends Laya.Sprite {
 			var shakeSpeed = 110; //设置阈值
 			//编写摇一摇方法
 			function shake(acceleration) {
-				var nowTime = new Date().getTime(); //记录当前时间
+				let nowTime = new Date().getTime(); //记录当前时间
 				//如果这次摇的时间距离上次摇的时间有一定间隔 才执行
 				if (nowTime - lastTime > 100) {
-					var diffTime = nowTime - lastTime; //记录时间段
+					let diffTime = nowTime - lastTime; //记录时间段
 					lastTime = nowTime; //记录本次摇动时间，为下次计算摇动时间做准备
 					x = acceleration.x; //获取x轴数值，x轴为垂直于北轴，向东为正
 					y = acceleration.y; //获取y轴数值，y轴向正北为正
 					z = acceleration.z; //获取z轴数值，z轴垂直于地面，向上为正
 					//计算 公式的意思是 单位时间内运动的路程，即为我们想要的速度
-					var speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000;
+					let speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000;
 					//console.log(speed)
 					if (speed > shakeSpeed) { //如果计算出来的速度超过了阈值，那么就算作用户成功摇一摇
 						shaked = true;
 						wx.stopAccelerometer();
 						fishList.forEach(fish=> {
-							fish.speed = 15;
+							fish.speed = 30;
 						});
 						setTimeout(()=> {
 							shaked = false;
